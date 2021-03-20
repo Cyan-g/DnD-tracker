b59601369adb0dd9c54e96b881c7dd94c2fdbac5
BuildTools <unconfigured@null.spigotmc.org>
2021-03-20 23:10:18 +0100

    misc

--- src/App.vue
+++ src/App.vue
@@ -168,14 +168,14 @@
                 <img
                   class="ml-1"
                   style="border-radius: 50%; height: 1.5rem"
-                  src="./Mystic_Coin.png"
+                  src="Mystic_Coin.png"
               /></b-row>
               <b-row
                 >Rest: {{ calcLeftover().rest }}
                 <img
                   class="ml-1"
                   style="border-radius: 50%; height: 1.5rem"
-                  src="./Mystic_Coin.png"/></b-row></b-col
+                  src="Mystic_Coin.png"/></b-row></b-col
             ><b-col v-for="(player, index) in players" :key="index">
               <b-form-input v-model="players[index].name"></b-form-input>
               <div class="mt-2">
@@ -185,7 +185,7 @@
                 <img
                   class="mb-3 ml-1"
                   style="border-radius: 50%; height: 2rem"
-                  src="./img/Mystic_Coin.png"
+                  src="Mystic_Coin.png"
                 />
               </div>
             </b-col>
